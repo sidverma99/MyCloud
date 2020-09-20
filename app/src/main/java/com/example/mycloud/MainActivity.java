2d6f27e8b4201f42d1abdbda52d25f7095e566c0
siddharth <sidverma24@gmail.com>
2020-09-20 16:12:01 +0530

    first commit

--- app/src/main/java/com/example/mycloud/MainActivity.java
+++ app/src/main/java/com/example/mycloud/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.mycloud;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+    private FloatingActionButton fab;
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        fab=(FloatingActionButton) findViewById(R.id.fab);
+        mRecyclerView=(RecyclerView)findViewById(R.id.recyclerView);
+
     }
 }
